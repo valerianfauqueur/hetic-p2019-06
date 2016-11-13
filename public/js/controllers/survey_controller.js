32470ea09e47dcf9a285ba65f375e5183c510991
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-11-13 13:15:44 +0100

    [survey:js] Add routing logic

--- public/js/controllers/survey_controller.js
+++ public/js/controllers/survey_controller.js
@@ -1,4 +1,19 @@
 import InputSlider from '../components/InputSlider';
 
-const ageInputsContainer = document.querySelector('.survey .survey__body .survey__age');
-const initAgeInputs = new InputSlider(ageInputsContainer, 'age__input', [21, 22, 23, 24, 25], 3, 1, 'y');
+const pathname = window.location.pathname;
+switch (pathname) {
+  case '/survey/question-1': {
+    const ageInputsContainer = document.querySelector('.survey .survey__body .survey__age');
+    const initAgeInputs = new InputSlider(ageInputsContainer, 'age__input', [21, 22, 23, 24, 25], 3, 1, 'y');
+    break;
+  }
+
+  case '/survey/question-2': {
+    const heightInputsContainer = document.querySelector('.survey .survey__body .survey__height');
+    const initHeightInputs = new InputSlider(heightInputsContainer, 'height__input', [165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175], 6, 1, 'y');
+  }
+
+  default: {
+    // nothing
+  }
+}
