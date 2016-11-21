babf5ac6fe64ecc6e199c30746b669158d9bfcf8
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-11-21 18:39:34 +0100

    [survey:controller] Add next question behaviour

--- public/js/controllers/survey_controller.js
+++ public/js/controllers/survey_controller.js
@@ -1,13 +1,33 @@
 import Hammer from 'hammerjs';
 import InputSlider from '../components/InputSlider';
 
+
+function switchQuestion(e) {
+  if (e.target.classList.contains('survey__introductionCTA')) {
+    const surveyIntroEl = e.target.parentNode;
+    const nextQuestionEl = document.querySelector('.survey .survey__question[data-question="1"]');
+    surveyIntroEl.classList.add('survey__introduction--over');
+    nextQuestionEl.classList.add('survey__question--current');
+  } else {
+    const nextQuestionNumber = parseInt(e.target.dataset.next, 10);
+    const currentQuestionNumber = parseInt(e.target.parentNode.dataset.question, 10);
+    const currentQuestionEl = document.querySelector(`.survey .survey__question[data-question="${currentQuestionNumber}"]`);
+    const nextQuestionEl = document.querySelector(`.survey .survey__question[data-question="${nextQuestionNumber}"]`);
+    // Change the current question
+    currentQuestionEl.classList.remove('survey__question--current');
+    nextQuestionEl.classList.add('survey__question--current');
+  }
+}
+
 const pathname = window.location.pathname;
 if (pathname === '/') {
   // Create input slider for age question
   const ageInputsContainer = document.querySelector('.survey .survey__question--age .survey__questionAnswerInputs');
+  const ageEventContainer = document.querySelector('.survey .survey__question--age .survey__questionAnswer');
   const initAgeInputs = new InputSlider(
     ageInputsContainer,
     'survey__questionAnswerInput',
+    ageEventContainer,
     [21, 22, 23, 24, 25],
     2,
     1,
@@ -23,9 +43,11 @@ if (pathname === '/') {
 
   // Create input slider for height question
   const heightInputsContainer = document.querySelector('.survey .survey__question--height .survey__questionAnswerInputs');
+  const heightEventContainer = document.querySelector('.survey .survey__question--height .survey__questionAnswer');
   const initHeightInputs = new InputSlider(
     heightInputsContainer,
     'survey__questionAnswerInput',
+    heightEventContainer,
     [165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175],
     5,
     1,
@@ -41,9 +63,11 @@ if (pathname === '/') {
   // Create input slider for weight question
   const weightInputsContainer = document.querySelector('.survey .survey__question--weight .survey__questionAnswerInputs');
   const weightDisplay = document.querySelector('.survey .survey__question--weight .survey__questionAnswerDisplay');
+  const weightEventContainer = document.querySelector('.survey .survey__question--weight .survey__questionAnswer');
   const initWeightInputs = new InputSlider(
     weightInputsContainer,
     'survey__questionAnswerInput',
+    weightEventContainer,
     [49.5, 50, 50.5, 51, 51.5, 52, 52.5, 53.5, 54, 54.5, 55,
       55.5, 56, 56.5, 57, 57.5, 58, 58.5, 59, 59.5, 60],
     10,
@@ -87,4 +111,22 @@ if (pathname === '/') {
       e.target.classList.add('survey__questionAnswerInput--selected');
     }
   }, true);
+
+
+  // Handle question transition
+  const nextContainers = document.querySelectorAll('.survey .survey__question .survey__questionValidation');
+
+  for (let i = 0, c = nextContainers.length; i < c; i += 1) {
+    const nextContainer = new Hammer.Manager(nextContainers[i]);
+    const nextTap = new Hammer.Tap();
+    nextContainer.add(nextTap);
+    nextContainer.on('tap', switchQuestion);
+  }
+
+  // Handle begin CTA
+  const ctaEl = document.querySelector('.survey .survey__introduction .survey__introductionCTA');
+  const ctaEvent = new Hammer.Manager(ctaEl);
+  const nextTap = new Hammer.Tap();
+  ctaEvent.add(nextTap);
+  ctaEvent.on('tap', switchQuestion);
 }
