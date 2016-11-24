9282e42fa856067c3c9b2121ad34a7feab9992ed
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-11-24 03:41:01 +0100

    [components:js] Fix slider class

--- public/js/components/Slider.js
+++ public/js/components/Slider.js
@@ -4,14 +4,16 @@ export default class Slider {
 
   constructor(container) {
     this.container = container;
+
+    this.slides = this.container.querySelectorAll('.slider__slide');
+    this.controls = this.container.querySelectorAll('.slider__controls .slider__controlPoint');
     this.initEvent();
   }
 
   initEvent() {
-    this.slidePoints = this.container.querySelectorAll('.slide__controls .slider__controlPoint');
     const goToSlideBinded = this.goToSlide.bind(this);
-    for (let i = 0, l = this.slidePoints.length; i < l; i += 1) {
-      const tapEvent = new Hammer.Manager(this.slidePoints[i]);
+    for (let i = 0, l = this.slides.length; i < l; i += 1) {
+      const tapEvent = new Hammer.Manager(this.controls[i]);
       const Tap = new Hammer.Tap();
       tapEvent.add(Tap);
       tapEvent.on('tap', this.goToSlide);
@@ -26,22 +28,42 @@ export default class Slider {
 
   goToSlide(e) {
     const currentlySelectedSlide = this.container.querySelector('.slider__slide--active');
-
-    if (e.target !== this.container) {
+    const currentControl = this.container.querySelector('.slider__controlPoint--active');
+    if (e.target.parentNode !== this.container) {
       const nextSlideNum = e.target.dataset.slide;
       const nextSlide = this.container.querySelector(`.slider__slide[data-slide="${nextSlideNum}"]`);
 
       currentlySelectedSlide.classList.remove('slider__slide--active');
       nextSlide.classList.add('slider__slide--active');
-    } else if (e.target === this.container) {
+    } else if (e.target.parentNode === this.container) {
       if (e.type === 'swipeleft') {
-        const nextSelectedSlide = currentlySelectedSlide.previousElementSibling();
+        let nextSelectedSlide = null;
+        let nextControl = null;
+        if (this.slides[this.slides.length - 1] === currentlySelectedSlide) {
+          nextSelectedSlide = this.slides[0];
+          nextControl = this.controls[0];
+        } else {
+          nextSelectedSlide = currentlySelectedSlide.nextElementSibling;
+          nextControl = currentControl.nextElementSibling;
+        }
         currentlySelectedSlide.classList.remove('slider__slide--active');
         nextSelectedSlide.classList.add('slider__slide--active');
+        currentControl.classList.remove('slider__controlPoint--active');
+        nextControl.classList.add('slider__controlPoint--active');
       } else if (e.type === 'swiperight') {
-        const nextSelectedSlide = currentlySelectedSlide.nextElementSibling();
+        let nextSelectedSlide = null;
+        let nextControl = null;
+        if (this.slides[0] === currentlySelectedSlide) {
+          nextSelectedSlide = this.slides[this.slides.length - 1];
+          nextControl = this.controls[this.slides.length - 1];
+        } else {
+          nextSelectedSlide = currentlySelectedSlide.previousElementSibling;
+          nextControl = currentControl.previousElementSibling;
+        }
         currentlySelectedSlide.classList.remove('slider__slide--active');
         nextSelectedSlide.classList.add('slider__slide--active');
+        currentControl.classList.remove('slider__controlPoint--active');
+        nextControl.classList.add('slider__controlPoint--active');
       }
     }
   }
--- public/js/controllers/home_controller.js
+++ public/js/controllers/home_controller.js
@@ -2,7 +2,7 @@ import Slider from '../components/Slider';
 
 const pathname = window.location.pathname;
 if (pathname === '/home') {
-  const sliders = document.querySelectorAll('.slide_slider .slider__slide');
+  const sliders = document.querySelectorAll('.slide .slide__content .slider');
   const slider1 = new Slider(sliders[0]);
   const slider2 = new Slider(sliders[1]);
 }
